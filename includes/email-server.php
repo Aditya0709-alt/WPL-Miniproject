52b27b39a4a0411147a0bde063e1f6c1e7dda92a
Aditya Pawar <77115883+Aditya0709-alt@users.noreply.github.com>
2022-05-01 14:32:56 +0530

    changed footer

---
+++ includes/email-server.php
@@ -0,0 +1,7 @@
+<?php
+
+$SMTPuser = 'aditya.pawar2@somaiya.edu';   
+$SMTPpwd = 'Asura@4003'; 
+$SMTPtitle = "KLiK inc.";
+$Domain = 'localhost';
+
---
+++ includes/footer.php
@@ -0,0 +1,38 @@
+
+<br><br><br><br>
+<footer id="myFooter" class="mt-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3">
+                    <h2 class="logo"><a href="index.php"><img src='img/200.png'></a></h2>
+                </div>
+                <div class="col-sm-2">
+                    <h5>Get started</h5>
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="team.php">The Team</a></li>
+                        <li><a href="forum.php">The KLiK Forum</a></li>
+                        <li><a href="hub.php">The KLiK Hub</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-2">
+                    <h5>About us</h5>
+                    <ul>
+                        <li><a href="_klik creators/klik_saad.php">Aditya Pawar</a></li>
+                        <li><a href="_klik creators/klik_anas-imran.php">Aryaman Gandhi</a></li>
+                        <li><a href="_klik creators/klik_anas-kamal.php">Harshit Wadhavkar</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-2">
+                    <h5>Support</h5>
+                    <ul>
+                        <li><a href="contact.php">Contact Us</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3">
+
+                </div>
+            </div>
+        </div>
+
+    </footer>
